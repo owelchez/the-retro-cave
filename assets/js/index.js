24b56d50f163f10fa501d53673b364a69ebd2f37
Orvin Welchez <zerox504@Orvins-MacBook-Pro.local>
2020-04-21 16:23:56 -0400

    Dynamic owner's age in the DOM

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,12 @@
+// Update owner's age in the DOM
+function getAge(){
+  let s = new Date();
+  return s.getFullYear() - 1984;
+}
+
+document.getElementById("owner-age").innerHTML = getAge();
+
+// This flips the business cards images
 $(function(){
     $(".flip").flip({
         trigger: 'hover'
